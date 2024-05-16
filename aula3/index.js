8d95e9715f1cab6b83f324504c882bdb934a79e7
SaintArthurG <cazooklol@gmail.com>
2024-05-16 18:55:08 -0300

    Initial commit

---
+++ aula3/index.js
@@ -0,0 +1,9 @@
+//USO DAS ASPAS EM UM CONSOLE.LOG
+// ASPAS DUPLAS, ASPAS SIMPLES E CRASE
+
+console.log("Eu sou haut"); //String
+console.log('Eu sou haut'); 
+console.log("Eu sou haut");
+console.log(`"Eu sou 'haut'"`);
+
+console.log(2)
