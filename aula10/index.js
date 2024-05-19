de3b8ac85f8a2d258c15f65a8b720f5fe0a84e3a
SaintArthurG <cazooklol@gmail.com>
2024-05-18 21:37:28 -0300

    primite types

---
+++ aula10/index.js
@@ -0,0 +1,26 @@
+//Aritmeticos + - / * **
+// + Adição / Concatenação
+// ** Potenciação
+// % Resto da divisão
+// incremento ++
+// decremento --
+
+// NaN == Not a Number
+
+// operadores de atribuição += -= *= /=
+const num1 = 10;
+const num2 = 2;
+const num3 = Number("");
+
+let contador = 1;
+
+contador++;
+ 
+console.log(contador);
+const passo = 5;
+contador += passo;
+console.log(contador)
+console.log(num1 * num3)
+
+
+
