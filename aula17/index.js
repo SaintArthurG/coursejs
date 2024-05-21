3ffc0e0461ab01ab659935dc62574a7a010a154e
SaintArthurG <cazooklol@gmail.com>
2024-05-21 17:28:51 -0300

    arrays

---
+++ aula17/index.js
@@ -0,0 +1,41 @@
+//                0        1       2
+const alunos = ["Luiz", "Maria", "João"];
+
+
+console.log(alunos.slice(0, -1)); // pega o lenght e vai ate faltar 1
+
+
+// console.log(alunos[50]); // ele joga undefined.
+
+
+// delete alunos[1]; // remove mas nao tira o indice
+// console.log(alunos);
+
+
+// const removido = alunos.shift(); //remove o primeiro
+// console.log(removido);
+// console.log(alunos);
+
+
+
+
+// const removido = alunos.pop(); // remove do fim do array; e armazena na constante 'removido';
+
+// console.log(removido);
+
+// console.log(alunos);
+
+
+
+// alunos.unshift("Luiza"); // Adiciona no começo
+
+// console.log(alunos);
+
+
+
+//alunos.push("Otavio"); || alunos[alunos.length] = "Luiza"; // adiciona Luiza no ARRAY no final
+
+// alunos[0]= "Eduardo"; // Luiz = Eduardo
+
+
+//console.log(alunos[0]) // Luiz
