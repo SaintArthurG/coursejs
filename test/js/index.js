a0165e5a7809616f69d268bfca85ec573734f0a2
SaintArthurG <cazooklol@gmail.com>
2024-05-22 18:58:32 -0300

    continuando o exercicio

---
+++ test/js/index.js
@@ -0,0 +1,31 @@
+function meuScoop(){
+    const form = document.querySelector(".form");
+
+    form.onsubmit = function (evento){
+        alert(1);
+    };
+}
+
+
+
+
+
+
+// const nome = document.getElementById("nome");
+// const sobrenome = document.getElementById("sobrenome");
+// const peso = document.getElementById("peso");
+// const altura = document.getElementById("altura");
+// const enviar = document.getElementById("enviar");
+
+// const paragrafo = document.getElementById("paragrafo");
+
+// enviar.addEventListener("click", gravar)
+// gravar.addEventListener("read")
+
+
+// function gravar(){
+//     let array = [nome, sobrenome, peso, altura]
+//     this.nome = nome;    
+//     paragrafo.innerHTML += `${array.nome}`;
+// }
+
