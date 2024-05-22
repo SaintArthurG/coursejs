d819be0835df740bf1b8eb8882b3ecb2c1868094
SaintArthurG <cazooklol@gmail.com>
2024-05-22 15:16:55 -0300

    functions

---
+++ aula18/index.js
@@ -0,0 +1,27 @@
+
+const raiz = n => n ** 0.5;         // arrow function
+
+
+// const raiz = function (n){         //função anonima 
+//     return n ** 0.5;
+// };                                 // fim da função anonima
+// console.log(raiz(9));
+// console.log(raiz(16));
+// console.log(raiz(25));
+
+// function soma (x = 0, y = 0){
+//     return x + y;
+// }                                
+
+
+
+
+// console.log(soma( ));
+
+// function saudacao(nome){
+//     return `Bom dia ${nome}!`;
+// }
+
+// let nome = "Arthur";
+// const variavel = saudacao(nome);
+// console.log(variavel);
