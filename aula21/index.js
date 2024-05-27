296e5131df7a8298b6dbc9d03f6f13944a42258e
SaintArthurG <cazooklol@gmail.com>
2024-05-27 15:29:32 -0300

    operadores logicos e operadores de comparação

--- aula21/index.js
+++ aula21/index.js
@@ -1,57 +1,15 @@
+/*
+Operadores de comparação
+> maior que
+>= maior ou igual a
+< menor que
+<= menor ou igual a
+= atribuição
+=== igualdade estrita (valor e tipo)
+!== diferente estrito (valor e tipo)
+*/
 
-const pessoa1 = {
-    nome: "Arthur",
-    sobrenome: "Godoy",
-    idade: 23,
-
-    falaNome(){
-        console.log(`Meu nome é ${this.nome} ${this.sobrenome}`)
-    },
-
-    falaIdade(){
-        console.log(`Eu tenho ${this.idade} anos`)
-    }
-};
-
-pessoa1.falaNome();
-pessoa1.falaIdade();
-
-
-
-// function criaPessoa (nome, sobrenome, idade){
-//     return {
-//         nome: nome,
-//         sobrenome: sobrenome,
-//         idade: idade
-//     };
-// }
-
-// function criaPessoa (nome, sobrenome, idade){
-//     return {nome, sobrenome, idade};
-// }
-
-
-// const pessoa1 = criaPessoa("Arthur", "Godoy", 23);
-
-// console.log(pessoa1.nome, pessoa1.sobrenome, pessoa1.idade);
-
-
-
-
-
-
-// const array = [1,2,3];
-// array.push(4);
-// array[0] = "Luiz"
-// console.log(array);
-
-// const pessoa = {       //Object
-//     nome: "Arthur",
-//     sobrenome: "Godoy",
-//     idade: 23
-// };
-// console.log(pessoa.nome);
-// console.log(pessoa.sobrenome);
-// console.log(pessoa.idade);
-// 
-// 
+let num1 = 10;
+let num2 = "10";
+let comp = num1 !== num2;
+console.log(comp);
---
+++ aula22/index.js
@@ -0,0 +1,8 @@
+/*
+OPERADORES LOGICOS
+&& -> AND -> E // todas as expressoes precisam ser verdadeiras para retornar true
+|| -> OR -> OU // Uma das expressoes precisam ser verdadeiras para retornar true
+! -> NOT -> NÃO // Nega a expressão
+*/
+console.log(true || false);
+console.log(!!!!!!!!!!!!!!!true);
