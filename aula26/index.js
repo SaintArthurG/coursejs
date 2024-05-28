62ebfc45c2b7fa733f3a97558d777a213a3da286
SaintArthurG <cazooklol@gmail.com>
2024-05-28 18:19:12 -0300

    operador ternario

---
+++ aula26/index.js
@@ -0,0 +1,7 @@
+// operador ternario
+// (condição) ? 'valor para verdadeiro' : 'valor para falso';
+const pontos = 1000;
+
+const usuario = pontos >= 1000 ? "Usuário VIP" : "Usuário normal";
+console.log(usuario); 
+
