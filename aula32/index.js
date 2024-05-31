23a4f151cbf371998acec28d6830f97e7d4a195c
SaintArthurG <cazooklol@gmail.com>
2024-05-31 20:13:38 -0300

    laço de repeticção for classico

---
+++ aula32/index.js
@@ -0,0 +1,11 @@
+// ## estrutura de repetição em javaScript
+
+//  #i = index
+// for(let i = 1; i <= 5; i++){
+//     console.log(`Linha ${i}`);
+// }
+
+const frutas = ["Maça", "Pêra", "Morango", "Uva"];
+for(let i = 0; i < frutas.length; i++){
+    console.log(`Indice ${i} ${frutas}`);
+}
