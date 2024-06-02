ad94989e4c24acbc587b9f1d42dd22b3ea2be054
SaintArthurG <cazooklol@gmail.com>
2024-06-01 21:12:53 -0300

    solucao mais rapida com for

--- aula33/assets/js/main.js
+++ aula33/assets/js/main.js
@@ -5,41 +5,15 @@ function scooby() {
         { tag: "footer", texto: "Frase3" },
         { tag: "section", texto: "Frase4" }
     ];
-    const [{ tag: tagP, texto: textoP }, { tag: tagDiv, texto: textoDiv }, { tag: tagFooter, texto: textoFooter }, { tag: tagSection, texto: textoSection }] = elementos;
+    const container = document.querySelector(".container");
+   
+    for(let i = 0; i < elementos.length; i++){
+        let { tag , texto } = elementos[i];
+        let ele = document.createElement(tag);
+        ele.innerHTML += texto;
+        container.appendChild(ele);
+    }
 
-    function createAF(tag) {
-        const section = document.querySelector(".container"); 
-        switch (tag) {
-            case "p":
-                const p = document.createElement(tagP);
-                p.innerHTML += textoP;
-                section.appendChild(p);
-                return p;
-            case "div":
-                const div = document.createElement(tagDiv);
-                div.innerHTML += textoDiv;
-                section.appendChild(div);
-                return div;
-            case "footer":
-                const footer = document.createElement(tagFooter);         
-                footer.innerHTML += textoFooter;
-                section.appendChild(footer);
-                return footer;
-            case "section":
-                const sectionn = document.createElement(tagSection);
-                sectionn.innerHTML += textoSection;
-                section.appendChild(sectionn);
-                return section;
-            default:
-                console.log("something goes wrong");
-                break;
-        }
-    };
-    
-    createAF(tagP);
-    createAF(tagDiv);
-    createAF(tagFooter);
-    createAF(tagSection);
     
 }
 scooby();
